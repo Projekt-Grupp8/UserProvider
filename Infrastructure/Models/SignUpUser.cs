dd43cf6d88b0deb2e55a1ed6b79c6af39dba4cbc
TPieplow <ted.pieplow@gmail.com>
2024-10-16 14:37:23 +0200

    SignUpUser Model created

--- Infrastructure/Models/SignUpUser.cs
+++ Infrastructure/Models/SignUpUser.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Infrastructure.Models;
 
-namespace Infrastructure.Models
+public class SignUpUser
 {
-    internal class SignUpUser
-    {
-    }
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public bool TermsConfirmed { get; set; } = false;
 }
