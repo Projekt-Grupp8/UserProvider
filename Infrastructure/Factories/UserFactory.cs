068e152096855ded013d6ea90fb7620359b5053b
TPieplow <ted.pieplow@gmail.com>
2024-10-16 14:45:15 +0200

    Added factory for signupmodel

--- Infrastructure/Factories/UserFactory.cs
+++ Infrastructure/Factories/UserFactory.cs
@@ -1,11 +1,29 @@
 ﻿using Infrastructure.Entities;
+using Infrastructure.Models;
+using System.Diagnostics;
 
 namespace Infrastructure.Factories;
 
 public class UserFactory
 {
     // Entitet => Model
-
-    // Model => Entitet
-
+    public static ApplicationUser Create(SignUpUser model)
+    {
+        try
+        {
+            return new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                Created = DateTime.UtcNow,
+                Updated = DateTime.UtcNow,
+            };
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"ApplicationUser Create:: ERROR: {ex.Message}");
+            return null!;
+        }
+    }
+  
 }
