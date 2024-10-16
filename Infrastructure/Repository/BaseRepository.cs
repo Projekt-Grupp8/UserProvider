743cc19c78de5fa79a886205f53e2fab920a9fbc
TPieplow <ted.pieplow@gmail.com>
2024-10-16 16:13:28 +0200

    BaseRepository done

--- Infrastructure/Repository/BaseRepository.cs
+++ Infrastructure/Repository/BaseRepository.cs
@@ -1,5 +1,77 @@
-﻿namespace Infrastructure.Repository;
+﻿using Infrastructure.Data;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
-public class BaseRepository
+namespace Infrastructure.Repository;
+
+public abstract class BaseRepository<TEntity>(DataContext context) where TEntity : class
 {
+    private readonly DataContext _context = context;
+
+    public virtual async Task<ResponseResult> CreateAsync(TEntity entity)
+    {
+		try
+		{
+			_context.Set<TEntity>().Add(entity);
+			await _context.SaveChangesAsync();
+			return ResponseFactory.Ok(entity);
+		}
+		catch (Exception ex)
+		{
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+	public virtual async Task<ResponseResult> GetOneAsync(Expression<Func<TEntity, bool>> predicate)
+	{
+		try
+		{
+			var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+			if (result is null)
+			{
+				return ResponseFactory.NotFound();
+			}
+
+			return ResponseFactory.Ok(result);
+		}
+		catch (Exception ex)
+		{
+            return ResponseFactory.Error(ex.Message);
+        }
+	}
+
+	public virtual async Task<ResponseResult> GetAllAsync()
+	{
+		try
+		{
+			var result = _context.Set<TEntity>().ToListAsync();
+			return ResponseFactory.Ok(result);
+		}
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+	public virtual async Task<ResponseResult> UpdateAsync(Expression<Func<TEntity, bool>> predicate, TEntity newEntity)
+	{
+		try
+		{
+			var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+			if (result is not null)
+			{
+				 _context.Entry(result).CurrentValues.SetValues(newEntity);
+				await _context.SaveChangesAsync();
+				return ResponseFactory.Ok(newEntity);
+			}
+
+			return ResponseFactory.NotFound();
+		}
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
 }
