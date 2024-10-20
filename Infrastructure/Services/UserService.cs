23a5d4f45f7532e07f6f1169359558992865682e
TPieplow <ted.pieplow@gmail.com>
2024-10-20 11:43:47 +0200

    save

--- Infrastructure/Services/UserService.cs
+++ Infrastructure/Services/UserService.cs
@@ -4,12 +4,14 @@ using Infrastructure.Factories;
 using Infrastructure.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace Infrastructure.Services;
 
-public class UserService(UserManager<ApplicationUser> userManager, DataContext context)
+public class UserService(UserManager<ApplicationUser> userManager, DataContext context, ILogger<UserService> logger)
 {
+    private readonly ILogger<UserService> _logger = logger;
     private readonly UserManager<ApplicationUser> _userManager = userManager;
     private readonly DataContext _context = context;
 
@@ -24,12 +26,16 @@ public class UserService(UserManager<ApplicationUser> userManager, DataContext c
 
             var user = UserFactory.Create(model);
             var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                return ResponseFactory.InternalError();
+            }
 
             return ResponseFactory.Ok(user);
         }
         catch (Exception ex)
         {
-            Debug.WriteLine($"ERROR:: Registration failed: {ex.Message}");
+            _logger.LogError(ex, "<CreateUserSync> Registration failed.");
             return ResponseFactory.InternalError();
         }
     }
