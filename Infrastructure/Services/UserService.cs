cd168dfc6e04773ef7cf3e99366c34f6f7a4482e
TPieplow <ted.pieplow@gmail.com>
2024-10-20 10:56:34 +0200

    CreateUserAsync working, still need to add-migration

--- Infrastructure/Services/UserService.cs
+++ Infrastructure/Services/UserService.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infrastructure.Data;
+using Infrastructure.Entities;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
-namespace Infrastructure.Services
+namespace Infrastructure.Services;
+
+public class UserService(UserManager<ApplicationUser> userManager, DataContext context)
 {
-    internal class UserService
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly DataContext _context = context;
+
+    public async Task<ResponseResult> CreateUserAsync(SignUpUser model)
     {
+        try
+        {
+            if (await _context.Users.AnyAsync(x => x.Email == model.Email))
+            {
+                return ResponseFactory.Exists();
+            }
+
+            var user = UserFactory.Create(model);
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            return ResponseFactory.Ok(user);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"ERROR:: Registration failed: {ex.Message}");
+            return ResponseFactory.InternalError();
+        }
     }
 }
